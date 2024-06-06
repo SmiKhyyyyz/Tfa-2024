f573d27a40a702ad573b17b0bec9f4f9359de5c0
SmiKhyyyyz <119935653+SmiKhyyyyz@users.noreply.github.com>
2024-06-07 00:18:08 +0200

    suite medias

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -49,7 +49,7 @@ function mouseMoveHandler(e) {
 
     // Limiter le défilement à la valeur maximale
     if (newScrollLeft < 0) {
-        newScrollLeft = -275;
+        newScrollLeft = -375;
     } else if (newScrollLeft > maxScrollLeft) {
         newScrollLeft = maxScrollLeft;
     }
@@ -79,8 +79,14 @@ window.addEventListener('resize', () => {
 
 let items = document.querySelectorAll('.projets__slider .projets__container--items');
 let active = 0;
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
 
 function loadShow() {
+    if (window.innerWidth < 1000) {
+        return;
+    }
+
     items[active].style.transform = `none`;
     items[active].style.zIndex = 1;
     items[active].style.filter = 'none';
@@ -103,17 +109,119 @@ function loadShow() {
         items[i].style.opacity = stt > 2 ? 0 : 0.6;
     }
 }
+
+function updateEventListeners() {
+    if (window.innerWidth >= 1000) {
+        next.onclick = function () {
+            active = (active + 1) % items.length;
+            loadShow();
+        }
+
+        prev.onclick = function () {
+            active = (active - 1 + items.length) % items.length;
+            loadShow();
+        }
+    } else {
+        next.onclick = null;
+        prev.onclick = null;
+    }
+}
+
+window.addEventListener('resize', function() {
+    updateEventListeners();
+    loadShow();
+});
+
+updateEventListeners();
 loadShow();
 
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
 
-next.onclick = function () {
-    active = (active + 1) % items.length;
-    loadShow();
+
+
+
+
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 }
 
-prev.onclick = function () {
-    active = (active - 1 + items.length) % items.length;
-    loadShow();
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function getRandomDirection() {
+    return (Math.random() < 0.5 ? -1 : 1) * 0.2;
+}
+
+function getRadius() {
+    if (window.innerWidth <= 480) {
+        return { r1: 50, r2: 75, r3: 60, r4: 80 };
+    } else if (window.innerWidth <= 768) {
+        return { r1: 100, r2: 160, r3: 125, r4: 140 };
+    } else {
+        return { r1: 200, r2: 125, r3: 250, r4: 300 };
+    }
+}
+
+function initializeCircles() {
+    const radius = getRadius();
+    circles[0].radius = radius.r1;
+    circles[1].radius = radius.r2;
+    circles[2].radius = radius.r3;
+    circles[3].radius = radius.r4;
+
+    circles.forEach(circle => {
+        circle.x = getRandomInt(circle.radius, canvas.width - circle.radius);
+        circle.y = getRandomInt(circle.radius, canvas.height - circle.radius);
+    });
+}
+
+const circles = [
+    { radius: 200, dx: getRandomDirection(), dy: getRandomDirection(), zIndex: 6, color: 'rgba(141, 169, 196, 0.5)' },
+    { radius: 300, dx: getRandomDirection(), dy: getRandomDirection(), zIndex: 6, color: 'rgba(141, 169, 196, 0.8)' },
+    { radius: 250, dx: getRandomDirection(), dy: getRandomDirection(), zIndex: 6, color: 'rgba(141, 169, 196, 0.65)' },
+    { radius: 400, dx: getRandomDirection(), dy: getRandomDirection(), zIndex: 6, color: 'rgba(141, 169, 196, 0.35)' }
+];
+
+function drawCircle(circle) {
+    ctx.beginPath();
+    ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2);
+    ctx.fillStyle = circle.color;
+    ctx.fill();
+    ctx.closePath();
+}
+
+function updateCircle(circle) {
+    if (circle.x + circle.radius > canvas.width || circle.x - circle.radius < 0) {
+        circle.dx = -circle.dx;
+    }
+    if (circle.y + circle.radius > canvas.height || circle.y - circle.radius < 0) {
+        circle.dy = -circle.dy;
+    }
+
+    circle.x += circle.dx;
+    circle.y += circle.dy;
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    circles.sort((a, b) => a.zIndex - b.zIndex);
+    circles.forEach(circle => {
+        drawCircle(circle);
+        updateCircle(circle);
+    });
+    requestAnimationFrame(animate);
+}
+
+window.addEventListener('resize', () => {
+    resizeCanvas();
+    initializeCircles();
+});
+
+resizeCanvas();
+initializeCircles();
+animate();
